805d6fa8b08f57268a5635b21b36f5a888486e24
prahladverma100 <prahladverma7744@gmail.com>
2024-02-15 12:42:50 +0530

    update

--- src/component/About_dinol.jsx
+++ src/component/About_dinol.jsx
@@ -4,9 +4,9 @@ import tree from '../assets/image/webp/tree.webp'
 
 const About_dinol = () => {
     return (
-        <div className=' relative bg-[#09655A] overflow_hidden z-[2] mt-[-2px]'>
-            <img className=' right-0 lg:bottom-[-22%] bottom-[-14%] lg:max-w-[300px] max-w-[200px] w-full absolute z-[-1]' src={cartoon} alt="" />
-            <img className=' left-0 lg:bottom-[-25%] bottom-[-14%] lg:max-w-[207px] max-w-[120px] w-full absolute z-[-1]' src={tree} alt="" />
+        <div className=' relative bg-[#09655A] z-[2]    '>
+            <img className=' right-0 lg:bottom-[-22%] bottom-[-14%] lg:max-w-[300px]  max-w-[200px] w-full absolute z-[10]' src={cartoon} alt="" />
+            <img className=' left-0 lg:bottom-[-25%] bottom-[-14%] lg:max-w-[207px] max-w-[120px] w-full absolute z-[10]' src={tree} alt="" />
             <div className=' px-3 lg:pt-[76px] pt-0 pb-4' data-aos="zoom-in">
                 <h3 className='text-center lg:text-[64px] text-4xl leading-[83.63px] lg:pb-[42px] pb-0 font-normal text-white ff_chewy'>About <span className='ff_lucky fw-normal lg:text-[69.67px] text-4xl leading-[66.19px]  drop-shadow-[0px_2.800px_0px_#fff]'><span className='text-[#FBA11D]'>D</span><span className='text-[#23aaac]'>I</span><span className='text-[#d45b07]'>N</span><span className='text-[#8ec627]'>O</span><span className='text-[#8EC627]'>L</span><span className='text-[#FCCA2B]'>F</span><span className='text-[#8F78D2]'>G</span></span></h3>
                 <p className='ff_balsamiq font-normal lg:text-2xl md:text-base text-sm leading-7  text-white text-center max-w-[]'>  is a meme coin with very strong utility.</p>
--- src/component/Last_section.jsx
+++ src/component/Last_section.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import foter_img from '../assets/image/webp/foter_img.webp'
+import dinosour1 from '../assets/image/webp/dinosour1.webp'
 import { Discord1, Insta1, Tiwtter1 } from './Icon'
 
 const Last_section = () => {
     return (
         <div className=' bg-[#0A4740] overflow_hidden relative lg:pt-32 mt-[-2px] pt-0'>
+            <img className=' absolute bottom-[2%] z-20 right-[14%] md:right-[7%] w-full lg:max-w-[190px] md:max-w-[120px] max-w-[100px] animation_cortoon' src={dinosour1} alt="" />
             <div className=' w-full absolute bottom-0'>
                 <img className=' w-full ' src={foter_img} alt="" />
             </div>
-            <div className='lg:pb-[267px] pb-20 ' data-aos="zoom-in-up">
+            <div className='lg:pb-[267px] pb-44 ' data-aos="zoom-in-up">
                 <h3 className='ff_lucky fw-normal text-center text-5xl lg:text-[76.64px] leading-[76.85px] drop-shadow-[0px_2.745px_0px_#000] mb-0 '> <span className='text-[#FBA11D]'>D</span><span className='text-[#23aaac]'>I</span><span className='text-[#d45b07]'>N</span><span className='text-[#8ec627] '>O</span><span className='text-[#8EC627]'>L</span><span className='text-[#FCCA2B]'>F</span><span className='text-[#8F78D2]'>G</span></h3>
                 <p className=' ff_balsamiq font-normal text-sm leading-4 max-w-[376px] mx-auto text-center py-3 md:py-5 lg:py-[33px] text-white relative z-10'>Sed tempus pretium est, vestibulum dapibus mauris viverra id. Curabitur feugiat porta lorem,  </p>
                 <div className=' z-10 relative flex gap-2 justify-center'>
